5ee9c90257eb921b6ac0d3008f56d54c82ec50ca
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-30 19:47:37 +0300

    Add Job::Scaler

---
+++ models/job/processing/scaler.rb
@@ -0,0 +1,16 @@
+class Job::Scaler < Job::Processing
+
+  def perform
+    new_file_path = "#{self.output_dir}/#{SecureRandom.uuid}.txt"
+    File.open(new_file_path, "a") do |new_file|
+        file_path = self.input_files_array[0]
+        File.open(file_path) do |file|
+          new_file.write file.read
+          new_file.write "\nscaled to #{self.options[:scale]}!"
+        end
+    end
+    [new_file_path]
+  end
+
+end
+

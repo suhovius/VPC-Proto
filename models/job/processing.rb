a9471f1c6f05b32ca9756fa72dd4ff815c5049bb
Alexey Sukhoviy <martinsilenn@gmail.com>
2012-04-30 19:30:15 +0300

    Job::Concatenizer now returns and gets an array of files

--- models/job/processing.rb
+++ models/job/processing.rb
@@ -1,11 +1,11 @@
 class Job::Processing < Job::Base
 
-  attr :input_dir, :output_dir
+  attr :input_files_array, :output_dir
 
-  def initialize input_dir, output_dir, options = {}
+  def initialize input_files_array, output_dir, options = {}
     raise "Can't initialize an abstract class instance" if self.class == Job::Processing
     super(options)
-    @input_dir = input_dir
+    @input_files_array = input_files_array
     @output_dir = output_dir
   end
 
--- models/job/processing/concatenizer.rb
+++ models/job/processing/concatenizer.rb
@@ -1,7 +1,15 @@
 class Job::Concatenizer < Job::Processing
 
   def perform
-
+    new_file_path = "#{self.output_dir}/#{SecureRandom.uuid}.txt"
+    File.open(new_file_path, "a") do |new_file|
+      self.input_files_array.each do |file_path|
+        File.open(file_path) do |file|
+          new_file.write file.read
+        end
+      end
+    end
+    [new_file_path]
   end
 
 end
